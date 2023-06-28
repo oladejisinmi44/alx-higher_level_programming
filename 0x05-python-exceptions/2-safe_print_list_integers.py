922690c8763326d0da6e9e523be0535d0e9499ba
“oladejisinmi44” <oladejisinmi44@gmail.com>
2023-06-28 15:35:33 -0700

    workdone

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+def safe_print_list_integers(my_list=[], x=0):
+    j = 0
+    for i in range(x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            j += 1
+        except TypeError:
+            continue
+        except ValueError:
+            continue
+        except IndexError:
+            print("")
+            return j
+    print("")
+    return j
